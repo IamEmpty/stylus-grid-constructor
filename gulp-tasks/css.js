8983e8e4499105137f8398d5faf5752fe6182d45
pure-js <evolution.is.already.underway@gmail.com>
2016-11-21 07:22:49 +0300

    fix

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -1,9 +1,7 @@
-module.exports = (gulp, plugins, paths) =>
-  function () {
-    gulp.src(paths.stylus)
-      .pipe(plugins.plumber())
-      .pipe(plugins.sourcemaps.init())
-      .pipe(plugins.stylus())
-      .pipe(plugins.sourcemaps.write(''))
-      .pipe(gulp.dest(paths.build));
-  }
+module.exports = (gulp, plugins, paths) => () =>
+  gulp.src(paths.stylus)
+    .pipe(plugins.plumber())
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.stylus())
+    .pipe(plugins.sourcemaps.write(''))
+    .pipe(gulp.dest(paths.build))
